1894fb029b12a7e68a51e8181fd3f4484a292fe9
StrigZ <mirgasan200@gmail.com>
2026-03-04 20:14:28 +0300

    Create state to store settings; Create reset and update handlers;

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,22 @@
 import { ArrowButton } from 'src/ui/arrow-button';
-import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { useRef, useState } from 'react';
+import { FormEvent, useRef, useState } from 'react';
 import clsx from 'clsx';
 import { useClickOutside } from 'src/hooks/use-click-outside';
+import { Button } from 'src/ui/button';
+import { ArticleStateType } from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+type Props = {
+	settings: ArticleStateType;
+	resetForm: () => void;
+	updateSettings: (newSettings: ArticleStateType) => void;
+};
+export const ArticleParamsForm = ({
+	// settings,
+	// updateSettings,
+	resetForm,
+}: Props) => {
 	const [isOpen, setIsOpen] = useState(false);
 	const asideRef = useRef<HTMLElement>(null);
 	const arrowRef = useRef<HTMLDivElement>(null);
@@ -16,6 +26,12 @@ export const ArticleParamsForm = () => {
 		handler: () => setIsOpen(false),
 		ignoreRef: arrowRef,
 	});
+
+	const handleFormSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		// apply new settings
+		// setSettings(newSettings);
+	};
 
 	return (
 		<>
@@ -27,9 +43,14 @@ export const ArticleParamsForm = () => {
 			<aside
 				className={clsx(styles.container, { [styles.container_open]: isOpen })}
 				ref={asideRef}>
-				<form className={styles.form}>
+				<form className={styles.form} onSubmit={handleFormSubmit}>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='button'
+							type='clear'
+							onClick={resetForm}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
